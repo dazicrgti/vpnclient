4e321ce0b0c42daa3f3a143c7281db4cbb84963b
dazicrgti <146800893+dazicrgti@users.noreply.github.com>
2024-06-06 14:12:03 +0800

    Add files via upload

---
+++ vnetgtw-dazi-nps (5)/WindowsPowershell/VpnProfileSetup.ps1
@@ -0,0 +1,86 @@
+param(
+	[switch]$Force,
+	[switch]$MachineCertAuth
+)
+$EAP = '<EapHostConfig
+	xmlns="http://www.microsoft.com/provisioning/EapHostConfig">
+	<EapMethod>
+		<Type
+			xmlns="http://www.microsoft.com/provisioning/EapCommon">26
+		</Type>
+		<VendorId
+			xmlns="http://www.microsoft.com/provisioning/EapCommon">0
+		</VendorId>
+		<VendorType
+			xmlns="http://www.microsoft.com/provisioning/EapCommon">0
+		</VendorType>
+		<AuthorId
+			xmlns="http://www.microsoft.com/provisioning/EapCommon">0
+		</AuthorId>
+	</EapMethod>
+	<Config
+		xmlns="http://www.microsoft.com/provisioning/EapHostConfig">
+		<Eap
+			xmlns="http://www.microsoft.com/provisioning/BaseEapConnectionPropertiesV1">
+			<Type>26</Type>
+			<EapType
+				xmlns="http://www.microsoft.com/provisioning/MsChapV2ConnectionPropertiesV1">
+				<UseWinLogonCredentials>false</UseWinLogonCredentials>
+			</EapType>
+		</Eap>
+	</Config>
+</EapHostConfig>'
+
+$Connection = Get-VpnConnection -Name vnet-srh-ctu
+if($connection -ne $null)
+{
+	try
+    {
+     if ($Force -eq $false) {
+            $title = 'Confirm VPN update'
+            $question = 'There is a VPN connection with same name already present, do you want to rewrite it?'
+            $choices = '&Yes', '&No'
+            $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+            if ($decision -eq 1) {
+	            Write-Host "Exiting as update was rejected."
+	            exit
+	        }
+        }
+	    Remove-VpnConnection -Name vnet-srh-ctu -Force -ErrorAction Stop
+		Write-Host "Removed older version of the VPN connection"
+	}
+	catch
+	{
+		Write-Error "Error while Removing old connection: $_"
+		exit
+	}
+}
+try
+{
+    if ($MachineCertAuth -eq $false)
+    {
+        Add-VpnConnection -Name vnet-srh-ctu -ServerAddress azuregateway-5de00061-8a63-49a1-a782-b7b54dfeb06b-8916108f558c.vpn.azure.com -TunnelType Automatic -AuthenticationMethod Eap -SplitTunneling:$True -RememberCredential -EncryptionLevel Optional -EapConfigXmlStream $EAP -PassThru
+    } else {
+        Add-VpnConnection -Name vnet-srh-ctu -ServerAddress azuregateway-5de00061-8a63-49a1-a782-b7b54dfeb06b-8916108f558c.vpn.azure.com -TunnelType Automatic -AuthenticationMethod MachineCertificate -SplitTunneling:$True -RememberCredential -EncryptionLevel Optional -PassThru
+    }
+}
+catch
+{
+	Write-Error "Error while creating new connection: $_"
+	exit
+}
+
+try
+{
+	((Get-Content -Raw -path '~\AppData\Roaming\Microsoft\Network\Connections\Pbk\rasphone.pbk') -replace "(?s)(.*)DisableClassBasedDefaultRoute=0(.*)","`$1DisableClassBasedDefaultRoute=1`$2") | Set-Content -path '~\AppData\Roaming\Microsoft\Network\Connections\Pbk\rasphone.pbk'
+	((Get-Content -Raw -path '~\AppData\Roaming\Microsoft\Network\Connections\Pbk\rasphone.pbk') -replace "(?s)(.*)PlumbIKEv2TSAsRoutes=0(.*)","`$1PlumbIKEv2TSAsRoutes=1`$2") | Set-Content -path '~\AppData\Roaming\Microsoft\Network\Connections\Pbk\rasphone.pbk'
+	((Get-Content -Raw -path '~\AppData\Roaming\Microsoft\Network\Connections\Pbk\rasphone.pbk') -replace "(?s)(.*)AutoTiggerCapable=0(.*)","`$1AutoTiggerCapable=1`$2") | Set-Content -path '~\AppData\Roaming\Microsoft\Network\Connections\Pbk\rasphone.pbk'
+    Write-Host "Edited pbk file with required changes"
+}
+catch
+{
+	Write-Host "Error while editing the PBK file: $_"
+}
+
+Add-VpnConnectionRoute -ConnectionName vnet-srh-ctu -DestinationPrefix 172.26.0.64/26
+Add-VpnConnectionRoute -ConnectionName vnet-srh-ctu -DestinationPrefix 10.0.0.0/25
